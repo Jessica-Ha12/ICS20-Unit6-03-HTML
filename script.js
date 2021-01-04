488869f5b2732c6a7583e181b9ad8aa8c7bd3d0d
JessicaHa12 <jessica.ha1@ocsbstudent.ca>
2021-01-04 14:21:40 +0000

    js single quotes

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-let x = document.getElementById("myBtn");
-x.addEventListener("click", conclusion);
-x.addEventListener("click", myFunction);
-function conclusion() {
-  alert("Thank you for veritying your age!");
+const x = document.getElementById('myBtn');
+x.addEventListener('click', conclusion);
+x.addEventListener('click', myFunction);
+function conclusion () {
+  alert('Thank you for veritying your age!');
 }
-function myFunction() {
-  let age = document.getElementById("age").value;
+function myFunction () {
+  let age = document.getElementById('age').value;
   age = parseInt(age);
   let respond;
   if (age >= 17) {
-    respond = "You can watch an R-rated movie alone.";
+    respond = 'You can watch an R-rated movie alone.';
   } else if (age >= 13) {
-    respond = "You can watch a PG-13 movie alone.";
+    respond = 'You can watch a PG-13 movie alone.';
   } else if (age >= 5) {
-    respond = "You can watch a D-rated movie alone.";
+    respond = 'You can watch a D-rated movie alone.';
   } else {
     respond = "Uh... You're too young to for most things.";
   }

2250fc659b15c686648b13fd0424feb2d81051cc
JessicaHa12 <jessica.ha1@ocsbstudent.ca>
2021-01-04 14:47:06 +0000

    Removed semicolon in js

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
-const x = document.getElementById('mybtn');
-x.addEventListener('click', conclusion);
-x.addEventListener('click', myFunction);
+const x = document.getElementById('mybtn')
+x.addEventListener('click', conclusion)
+x.addEventListener('click', myFunction)
 function conclusion () {
-  alert('Thank you for veritying your age!');
+  alert('Thank you for veritying your age!')
 }
 function myFunction () {
-  let age = document.getElementById('age').value;
-  age = parseInt(age);
+  let age = document.getElementById('age').value
+  age = parseInt(age)
   let respond;
   if (age >= 17) {
-    respond = 'You can watch an R-rated movie alone.';
+    respond = 'You can watch an R-rated movie alone.'
   } else if (age >= 13) {
-    respond = 'You can watch a PG-13 movie alone.';
+    respond = 'You can watch a PG-13 movie alone.'
   } else if (age >= 5) {
-    respond = 'You can watch a D-rated movie alone.';
+    respond = 'You can watch a D-rated movie alone.'
   } else {
-    respond = "Uh... You're too young to for most things.";
+    respond = "Uh... You're too young to for most things."
   }
-  document.getElementById('demo').innerHTML = respond;
+  document.getElementById('demo').innerHTML = respond
 }
